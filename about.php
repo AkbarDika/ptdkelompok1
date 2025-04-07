185c169a1b00c0f4b717c438a192ccf2c756a547
AkbarDika <akbar.dika@mhs.itenas.ac.id>
2025-04-08 00:19:40 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+    <style>
+    </style>
+</head>
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bgnav">
+        <div class="container-fluid">
+            <a class="navbar-brand">Pengantar Sains Digital</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+                </li>
+                <!-- <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+                </li> -->
+            </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container-fluid row1" style="height: 500px;">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12 ">
+                <div class="aboutkiri">
+                    <h1>KELOMPOK 1</h1>
+                    <h5>Akbar Dika Abrisam 152024148</h5>
+                    <h5>Aditya Luthfi 152024139</h5>
+                    <h5>Alsani Abdul Rofiq 152024129</h5>
+                    <h5>Rafli Ramdhani 152024154</h5>
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-12 ">
+                <p class="aboutkanan">Website ini dibuat untuk memenuhi tugas mata kuliah Pengantar Sains Digital, website ini berisi informasi mengenai transformasi digital dalam bidang transportasi, tepatnya pada sistem ticketing kereta api indonesia</p>
+            </div>
+        </div>
+    </div>
+
+
+
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                    <div class="lfooter">
+                        <h4>Pengantar Sains Digital DD</h4>
+                        <p>Institut Teknologi Nasional Bandung</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <div class="rfooter">
+                        <h4>Kelompok 1</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <style>
+    </style>
+</head>
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bgnav">
+        <div class="container-fluid">
+            <a class="navbar-brand">Pengantar Sains Digital</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+                </li>
+                <!-- <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+                </li> -->
+            </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container-fluid row1">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12 ">
+                <h1 class="kiri">DIGITALISASI SISTEM TICKETING PADA KERETA API INDONESIA MENGGUNAKAN APLIKASI KAI ACCESS</h1>
+            </div>
+            <div class="col-lg-6 col-sm-12 ">
+                <img src="KAI_Access_logo-removebg-preview.png" alt="" class="img-fluid kanan">
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid row2">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="headtext1">APA ITU DIGITALISASI?</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="p1">Digitalisasi adalah proses mengubah data analog menjadi format digital. Digitalisasi dapat dilakukan pada berbagai aspek kehidupan, seperti dalam bisnis, pendidikan, dan administrasi. 
+                Digitalisasi merupakan bagian dari proses menuju transformasi digital. Transformasi digital mencakup seluruh aspek dalam bisnis, dan penerapannya bukan hanya tentang memanfaatkan teknologi. Industri transportasi mengalami perkembangan pesat berkat digitalisasi, termasuk dalam layanan pemesanan tiket kereta api. PT Kereta Api Indonesia (KAI) menghadirkan aplikasi KAI Access untuk meningkatkan efisiensi dan kenyamanan pengguna dalam melakukan pemesanan tiket secara online.
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container-fluid row3">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="headtext1">KAI ACCESS</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="p1">Industri transportasi mengalami perkembangan pesat berkat digitalisasi, termasuk dalam layanan pemesanan tiket kereta api. PT Kereta Api Indonesia (KAI) menghadirkan aplikasi KAI Access untuk meningkatkan efisiensi dan kenyamanan pengguna dalam melakukan pemesanan tiket secara online. Aplikasi ini menawarkan berbagai fitur yang mempermudah transaksi, namun masih memiliki beberapa kendala seperti kesulitan login, pemilihan jadwal yang kurang optimal, serta ketidakpastian ketersediaan tiket.</p>
+                <p class="p1">Sebelum digitalisasi, pemesanan tiket kereta api dilakukan secara manual di loket yang memakan waktu lama. Sejak peluncuran KAI Access pada 4 September 2022, pengguna dapat memesan tiket dengan lebih cepat dan fleksibel. Proses pemesanan tiket kini dapat dilakukan hanya dalam beberapa menit melalui perangkat seluler.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid row3 d-block d-md-none" style="height: 50px;"></div>
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6 col-sm-12">
+                <div class="boxkiri">
+                    <div class="row">
+                        <div class="col-12"><h3 style="text-align: center; margin-top: 20px;">Kelebihan</h3></div>
+                    </div>
+                    <div class="row" style="margin: 30px;">
+                        <p>- Hemat waktu dan tenaga karena tidak perlu antre</p>
+                        <p>- Bebas ribet karena bisa memesan tiket kapan saja dan di mana saja</p>
+                        <p>- Fleksibel karena bisa memilih tempat duduk selama masih tersedia</p>
+                        <p>- Tersedia beragam metode pembayaran</p>
+                        <p>- Tersedia fitur e-boarding pass sehingga tidak perlu mencetak tiket</p>
+                        <p>- Tersedia fitur “Live Delay Update” untuk mengetahui keterlambatan kedatangan kereta</p>
+                        <p>- Tersedia fitur “Riwayat Perjalanan” untuk melacak perjalanan</p>
+                        <p>- Kualitas pelayanan baik karena respon cepat dan tanggap</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-sm-12">
+                <div class="boxkanan">
+                    <div class="row">
+                        <div class="col-12"><h3 style="text-align: center; margin-top: 20px;">Kekurangan</h3></div>
+                    </div>
+                    <div class="row" style="margin: 30px;">
+                        <p>- Terkadang Lemot / Crash</p>
+                        <p>- Proses Refund Agak Lama</p>
+                        <p>- Butuh Koneksi Internet Stabil</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container-fluid row3">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="headtext1">KESIMPULAN</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <p class="p1">Digitalisasi melalui aplikasi KAI Access telah membawa perubahan besar dalam sistem pemesanan tiket kereta api. Namun, masih ada ruang untuk perbaikan guna meningkatkan pengalaman pengguna. Dengan menerapkan Business Process Reengineering, berbagai kendala dapat diatasi melalui inovasi teknologi seperti login biometrik, sistem rekomendasi jadwal, serta verifikasi tiket otomatis. Transformasi ini tidak hanya meningkatkan efisiensi layanan, tetapi juga mendukung era digitalisasi di sektor transportasi. Dengan perbaikan yang terus dilakukan, diharapkan aplikasi KAI Access dapat menjadi platform pemesanan tiket kereta api yang semakin andal dan nyaman bagi pengguna.        
+                </p>
+            </div>
+        </div>
+        <div class="container-fluid row3" style="height: 50px;"></div>
+    </div>
+
+    <footer>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                    <div class="lfooter">
+                        <h4>Pengantar Sains Digital DD</h4>
+                        <p>Institut Teknologi Nasional Bandung</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <div class="rfooter">
+                        <h4>Kelompok 1</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+
+
+</html>
